de2d7fa42231775b4e4d5d5673e7559107da8719
Rahul Pandare <pandare.rahul@gmail.com>
2025-06-11 11:38:44 -0700

    removed gitpush bash and text files from tracking

--- gitPush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git add .                        # Stages all new and modified files
-git commit -m "updated files"    # Commits the changes with a fixed message
-git push                         # Pushes changes to the master branch
-
-echo "✅ Changes successfully pushed to GitHub!"  # Displays a success message
